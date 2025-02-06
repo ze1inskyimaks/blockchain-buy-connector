c07a971b05508793ae63a868d4c5f238cb3986db
ze1inskyimaks <zelinskyimaks@gmail.com>
2025-02-06 13:40:41 +0200

    Refactor code to improve performance and readability

--- src/constants/contractConfig.ts
+++ src/constants/contractConfig.ts
@@ -1,180 +1,221 @@
-export const CONTRACT_ADDRESS = "0x19758F206CeE6a9D003CD5126Ff9672325561e27";
+export const CONTRACT_ADDRESS = "0xA52DDc36C1b7b3D649cEB1416448e8C03e531276";
 export const CONTRACT_ABI = [
   {
-    "inputs": [
-      {"internalType":"address","name":"initialOwner","type":"address"},
-      {"internalType":"address","name":"_tokenAddress","type":"address"},
-      {"internalType":"address","name":"_usdtAddress","type":"address"},
-      {"internalType":"uint256","name":"_tokenPriceUSDTinWei","type":"uint256"},
-      {"internalType":"uint256","name":"_hardCap","type":"uint256"},
-      {"internalType":"uint256","name":"_startTime","type":"uint256"},
-      {"internalType":"uint256","name":"_endTime","type":"uint256"}
+    inputs: [
+      { internalType: "address", name: "initialOwner", type: "address" },
+      { internalType: "address", name: "_tokenAddress", type: "address" },
+      { internalType: "address", name: "_usdtAddress", type: "address" },
+      {
+        internalType: "uint256",
+        name: "_tokenPriceUSDTinWei",
+        type: "uint256",
+      },
+      { internalType: "uint256", name: "_hardCap", type: "uint256" },
+      { internalType: "uint256", name: "_startTime", type: "uint256" },
+      { internalType: "uint256", name: "_endTime", type: "uint256" },
     ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
   {
-    "inputs": [{"internalType":"address","name":"owner","type":"address"}],
-    "name": "OwnableInvalidOwner",
-    "type": "error"
+    inputs: [{ internalType: "address", name: "owner", type: "address" }],
+    name: "OwnableInvalidOwner",
+    type: "error",
   },
   {
-    "inputs": [{"internalType":"address","name":"account","type":"address"}],
-    "name": "OwnableUnauthorizedAccount",
-    "type": "error"
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
   },
   {
-    "anonymous": false,
-    "inputs": [
-      {"indexed": false, "internalType": "address", "name": "owner", "type": "address"},
-      {"indexed": false, "internalType": "uint256", "name": "tokensUnsold", "type": "uint256"}
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokensUnsold",
+        type: "uint256",
+      },
     ],
-    "name": "ICOEnded",
-    "type": "event"
+    name: "ICOEnded",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
-      {"indexed": true, "internalType": "address", "name": "previousOwner", "type": "address"},
-      {"indexed": true, "internalType": "address", "name": "newOwner", "type": "address"}
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
     ],
-    "name": "OwnershipTransferred",
-    "type": "event"
+    name: "OwnershipTransferred",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
-      {"indexed": true, "internalType": "address", "name": "buyer", "type": "address"},
-      {"indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256"},
-      {"indexed": false, "internalType": "string", "name": "paymentMethod", "type": "string"}
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "buyer",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "paymentMethod",
+        type: "string",
+      },
     ],
-    "name": "TokensPurchased",
-    "type": "event"
+    name: "TokensPurchased",
+    type: "event",
   },
   {
-    "inputs": [{"internalType": "uint256", "name": "value", "type": "uint256"}],
-    "name": "GetAmountOfTokenForETH",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [{ internalType: "uint256", name: "value", type: "uint256" }],
+    name: "GetAmountOfTokenForETH",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [{"internalType": "uint256", "name": "value", "type": "uint256"}],
-    "name": "GetAmountOfTokenForUSDT",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [{ internalType: "uint256", name: "value", type: "uint256" }],
+    name: "GetAmountOfTokenForUSDT",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [{"internalType": "uint256", "name": "TokenPriceInUSDT", "type": "uint256"}],
-    "name": "GetTokenPriceInWeiForETH",
-    "outputs": [{"internalType": "uint256", "name": "priceInWei", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [
+      { internalType: "uint256", name: "TokenPriceInUSDT", type: "uint256" },
+    ],
+    name: "GetTokenPriceInWeiForETH",
+    outputs: [{ internalType: "uint256", name: "priceInWei", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "buyTokensWithETH",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
+    inputs: [],
+    name: "buyTokensWithETH",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
   },
   {
-    "inputs": [{"internalType": "uint256", "name": "usdtAmount", "type": "uint256"}],
-    "name": "buyTokensWithUSDT",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [{ internalType: "uint256", name: "usdtAmount", type: "uint256" }],
+    name: "buyTokensWithUSDT",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "tokenPriceUSDTinWei",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "tokenPriceUSDTinWei",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "decimals",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "endICO",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: "endICO",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "endTime",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "endTime",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "getLatestPrice",
-    "outputs": [{"internalType": "int256", "name": "", "type": "int256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "getLatestPrice",
+    outputs: [{ internalType: "int256", name: "", type: "int256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "hardCap",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "hardCap",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "renounceOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "startTime",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "startTime",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "token",
-    "outputs": [{"internalType": "contract IERC20", "name": "", "type": "address"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "token",
+    outputs: [{ internalType: "contract IERC20", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "tokensSold",
-    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [],
+    name: "tokensSold",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [{"internalType": "address", "name": "newOwner", "type": "address"}],
-    "name": "transferOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "usdt",
-    "outputs": [{"internalType": "contract IERC20", "name": "", "type": "address"}],
-    "stateMutability": "view",
-    "type": "function"
-  }
+    inputs: [],
+    name: "usdt",
+    outputs: [{ internalType: "contract IERC20", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
